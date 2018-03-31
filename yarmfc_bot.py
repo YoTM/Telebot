a4d383ea2e57852a07dd2919124678fad31f69a1
YoTM <yolkin_t@list.ru>
2018-03-31 14:53:24 +0300

    Добавил ф-ии получения ИД чата и отправки сообщения

--- yarmfc_bot.py
+++ yarmfc_bot.py
@@ -18,3 +18,23 @@ def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
     return results[total_updates]
+
+
+def get_chat_id(update):
+    """
+        Ф-я достаёт идентификатор чата из обновления
+    """
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+
+def send_mess(chat, text):
+    """
+        Ф-я отправки сообщения
+    """
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+send_mess(chat_id, 'Your message goes here')
