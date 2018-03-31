52664662d85cac301c22b516aebae19761d429ee
YoTM <yolkin_t@list.ru>
2018-03-31 15:00:43 +0300

    Добавлен основной цикл программы

--- yarmfc_bot.py
+++ yarmfc_bot.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 
 url = "https://api.telegram.org/bot583905412:AAGwYtlkfx0LxXfHue4IVZjKruHhzDohyB8/"
 
@@ -36,5 +37,17 @@ def send_mess(chat, text):
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
+
+def main():
+    """
+        НАЧАЛО программы
+    """
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url)))), 'test)
+            update_id += 1
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
