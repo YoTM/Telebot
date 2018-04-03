4a7b8af35919931d5615fd6d6e2d3fb990df5780
YoTM <yolkin_t@list.ru>
2018-04-03 18:16:26 +0300

    Добавил json-файл с контактами филиалов по Ярославлю Научил бота выдавать инфу по коду из списка филиалов

--- HH_bot.py
+++ HH_bot.py
@@ -1,49 +1,56 @@
+import json
 # Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
 updater = Updater(token='583905412:AAGwYtlkfx0LxXfHue4IVZjKruHhzDohyB8') # Токен API к Telegram
 dispatcher = updater.dispatcher
+
+
+def get_fil_info(id):
+
+    response = ""
+    with open('kontacts.json', 'r', encoding='UTF-8') as info:
+        data = json.loads(info.read())
+        for i in data[id]:
+            response = response + data[id][i] + '\n'
+    return response
+
 
 # Обработка команд
 def startCommand(bot, update):
     greeting = """
-
-        Приветствую!
-    Я официальный Телеграм бот от ГАО ЯО МФЦ.
-    Запущен в тестовом режиме.
-    Понимаю команды:
-    Раз -
-    Два -
-    Три -
-
+Приветствую!
+Я официальный Телеграм бот от ГАО ЯО МФЦ.
+Запущен в тестовом режиме.
+Информация по какому филиалу вам нужна?
+Введите номер для:
+1 - Ленинский р-н
+2 - Кировский р-н
+3 - Дзержинский р-н
+4 - Заволжский р-н 
     """
     bot.send_message(chat_id=update.message.chat_id, text=greeting)
 
 
 def textMessage(bot, update):
+    # Ведение лога вопросов к боту
     with open('input.txt', 'a') as f_log:
         f_log.write('-'*5 + 'new message' + '-'*5+'\n')
         f_log.write(update.message.text+'\n')
-    if update.message.text == 'загран паспорт':
-        response = """
-        Способы подачи заявки:
-            Лично
-            Через законного представителя
-            Через МФЦ
-        Способы получения результата:
-            Лично
-            Через законного представителя
-            Через МФЦ
-        Стоимость и порядок оплаты
-            Вид платежа:
-                Государственная пошлина
-            Стоимость:
-                2000.0
-        """
-    else:
-        response = 'Получил Ваше сообщение: ' + update.message.text
+        # f_log.write(update.message.location + '\n')
+
+    user_res = update.message.text
+    # print(user_res, type(user_res))
+    # print(user_res.isdigit())
+    try:
+        if user_res in ["0", "1", "2", "3", "4"]:
+            response = get_fil_info(int(user_res)-1)
+        else:
+            response = 'Получил Ваше сообщение: ' + update.message.text
+    except:
+        response = "error!"
     bot.send_message(chat_id=update.message.chat_id, text=response)
-
-
 
 
 # Хендлеры
